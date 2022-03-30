bd617c2858c120bfc124a4e4efda3efe4d641e78
Samuel Daba <58066840+samisancho@users.noreply.github.com>
2022-03-30 10:37:38 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,38 @@
 #include "main.h"
-
 /**
- * is_squre_root - determines the square root of
- * @j: an input value
- * @i: input value
+ *_sqrt - returns sqrt
  *
- * Description: determines the square root
- * Return: returns the input val
- */
-
-int is_square_root(int i, int j)
-{
-	if (j * j > i)
-	{
-		return (-1);
-	}
-	if (j * j == i)
-	{
-		return (j);
-	}
-	return (is_square_root(i, j + 1));
-}
-
-/**
- * _sqrt_recursion - returns square root
- * @n: input size
+ *@n: int param
+ *@i: value to be tested
  *
- * Description: If n does not have a natural square root,
- * the function should return -1
- * Return: returns the natural square root of a number
+ *Return:sqrt
+ *
  */
-
-int _sqrt_recursion(int n)
+int _sqrt(int n, int i)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return (is_square_root(n, 1));
+	if ((i * i) > n)
+	{
+		return (-1);
+	}
+	if ((i * i) == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
+/**
+ *_sqrt_recursion - returns square root of a natural number
+ *
+ *@n:number to find sqrt
+ *
+ *Return:sqrt,(-1) no natural sqrt
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
 }
