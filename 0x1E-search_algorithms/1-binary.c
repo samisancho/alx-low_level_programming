3d16c806b09738857bfdf617ad59179fce78c5a9
Samuel Daba <58066840+samisancho@users.noreply.github.com>
2023-02-28 16:01:38 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,33 @@
 #include "search_algos.h"
 
-void print_array(int *array, size_t i, size_t size);
-
 /**
- * binary_search - binary search algorithm in a string
- * @array: list of elements
- * @size: size of the list
- * @value: target to search
- * Return: element index on success (-1) on fail
+ * binary_search - funct that searches for a value in a sorted array iterative
+ * of integers using the Binary search algorithm
+ * @array: Type pointer of given array
+ * @size: Type size of elements in the array
+ * @value: Type value to be searched
+ * Return: Value, or -1 if value not present
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, half = 0;
+	int i, left, right;
 
-	if (array == NULL || value == 0)
+	if (!array)
 		return (-1);
-
-	print_array(array, low, size);
-	while (low < high)
+	for (left = 0, right = (int)size - 1; right >= left;)
 	{
-		half = (low + high) / 2;
-		if (array[half] < value)
-		{
-			low = half + 1;
-			print_array(array, low, high + 1);
-		}
-		else if (array[half] > value)
-		{
-			high = half - 1;
-			print_array(array, low, high + 1);
-		}
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
 		else
-			return (array[half]);
+			left = i + 1;
 	}
 	return (-1);
-}
-
-/**
- * print_array - print all elements in an array
- * @array: list of elements
- * @i: index to init
- * @size: size of the array
- */
-void print_array(int *array, size_t i, size_t size)
-{
-	printf("Searching in array: ");
-	while (i < size)
-	{
-		if (i + 1 == size)
-			printf("%d ", array[i]);
-		else
-			printf("%d, ", array[i]);
-		i++;
-	} printf("\n");
 }
